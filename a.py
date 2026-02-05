c5fd7d3c50f7a7edc1d3b026bc3eeba1fe5925e8
techboyz114 <techboyz114@gmail.com>
2026-02-05 19:42:24 +0530

    hi

---
+++ a.py
@@ -0,0 +1,17 @@
+import turtle
+t = turtle.Turtle()
+t.speed(0)
+turtle.bgcolor("White")
+t.pencolor("Black")
+
+for i in range(50):
+    t.forward(i * 5)
+    t.right(90)
+for i in range(50):
+    t.forward(i * 5)
+    t.right(90)
+for i in range(50):
+    t.forward(i * 5)
+    t.right(90)
+
+turtle.done()
